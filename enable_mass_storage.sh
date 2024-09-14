919448bdab3d53be506e3e45d0a63c978d3b81ee
Vincenzo Puca <32323498+VincePuc99@users.noreply.github.com>
2024-09-14 12:20:18 +0200

    Update enable_mass_storage.sh

--- enable_mass_storage.sh
+++ enable_mass_storage.sh
@@ -7,27 +7,53 @@ fi
 
 max_power=$1
 
-usb_gadget="/sys/kernel/config/usb_gadget"
-gadget="arlo"
-gadget_root="$usb_gadget/$gadget"
-img_file="/arlo.bin"
-lang=0x409
-cfg=c
-modprobe libcomposite
-mkdir -p "$gadget_root/configs/$cfg.1"
+usb_gadget="/sys/kernel/config/usb_gadget"  #Gadget DIR
+gadget="arlo"                               #Gadget Name
+gadget_root="$usb_gadget/$gadget"           #Gadget complete Path
+img_file="/arlo.bin"                        #IMG File Path
+lang=0x409                                  #English Lang Code
+cfg=c                                       #Config Name
+
+modprobe libcomposite                       #Load Lib For Gadgets
+
+mkdir -p "$gadget_root/configs/$cfg.1"      #Create gadget config dir
+
+#USB Gadget Specs
 echo 0x1d6b > "$gadget_root/idVendor"  # Linux Foundation
 echo 0x0104 > "$gadget_root/idProduct" # Composite Gadget
 echo 0x0100 > "$gadget_root/bcdDevice" # v1.0.0
 echo 0x0200 > "$gadget_root/bcdUSB"    # USB 2.0
+
+#More configs for gadget
 mkdir -p "$gadget_root/strings/$lang"
 mkdir -p "$gadget_root/configs/$cfg.1/strings/$lang"
+
+#Cpu serial number to gadget serial number
 echo "$(grep Serial /proc/cpuinfo | awk '{print $3}')" > "$gadget_root/strings/$lang/serialnumber"
+
+#Productor Gadget
 echo "TDD" > "$gadget_root/strings/$lang/manufacturer"
+
+#Product name
 echo "Composite Drive" > "$gadget_root/strings/$lang/product"
-echo "Pi Zero W" > "$gadget_root/configs/$cfg.1/strings/$lang/configuration"
+
+#Description of gadget 
+echo "Raspberry Pi Foundation" > "$gadget_root/configs/$cfg.1/strings/$lang/configuration"
+
+#Set max power
 echo "$max_power" > "$gadget_root/configs/$cfg.1/MaxPower"
+
+#Main dir for gadget data
 mkdir -p "$gadget_root/functions/mass_storage.0"
+
+#IMG as file for archiving data
 echo "$img_file" > "$gadget_root/functions/mass_storage.0/lun.0/file"
-echo "Arlo Pi $(du -h $img_file | awk '{print $1}')" > "$gadget_root/functions/mass_storage.0/lun.0/inquiry_string"
+
+#Device name
+echo "Arlo RPi $(du -h $img_file | awk '{print $1}')" > "$gadget_root/functions/mass_storage.0/lun.0/inquiry_string"
+
+#simlink
 ln -sf "$gadget_root/functions/mass_storage.0" "$gadget_root/configs/$cfg.1"
+
+#Gadget activation
 ls /sys/class/udc > "$gadget_root/UDC"
