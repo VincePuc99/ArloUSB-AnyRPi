2c394981f97f7d10e480787843dcfadf376a4a97
Vincenzo Puca <32323498+VincePuc99@users.noreply.github.com>
2024-09-19 16:35:22 +0200

    Update Arlo-Usb-Start.sh
    - Removed dependencies installation
    - Updated log creation

--- Arlo-Usb-Start.sh
+++ Arlo-Usb-Start.sh
@@ -87,9 +87,8 @@ dependencies=(bash findutils util-linux rsync grep coreutils procps kmod)
 
 for package in "${dependencies[@]}"; do
     if ! is_installed "$package"; then
-        echo "Installing missing dependency: $package"
-        sudo apt-get update
-        sudo apt-get install -y "$package"
+        echo "Dependencies ERROR: $package - 3/6">> "$LOG_FILE"
+        exit 1
     fi
 done
 
@@ -97,33 +96,54 @@ echo "Dependencies SUCCESS - 3/6">> "$LOG_FILE"
 
 ################################################################# DWC2
 
-echo "dwc2" >> /etc/modules
-echo "dtoverlay=dwc2" >> /boot/config.txt
+# Append "dwc2" to /etc/modules
+if sudo sh -c 'echo "dwc2" >> /etc/modules'; then
+    echo "Successfully appended 'dwc2' to /etc/modules" >> "$LOG_FILE"
+else
+    echo "Failed to append 'dwc2' to /etc/modules" >> "$LOG_FILE"
+    exit 1
+fi
 
+# Append "dtoverlay=dwc2" to /boot/config.txt
+if sudo sh -c 'echo "dtoverlay=dwc2" >> /boot/config.txt'; then
+    echo "Successfully appended 'dtoverlay=dwc2' to /boot/config.txt" >> "$LOG_FILE"
+else
+    echo "Failed to append 'dtoverlay=dwc2' to /boot/config.txt" >> "$LOG_FILE"
+    exit 1
+fi
+
+# Log the success message
 echo "DWC2 SUCCESS - 4/6" >> "$LOG_FILE"
 
 ################################################################# Storage
 
-ARLO_IMG_FILE=/arlo.bin
+ARLO_IMG_FILE=/arlo.bin     # Define the ARLO image file and its size
 ARLO_IMG_SIZE=31457280
+
+# Function to calculate the offset of the first partition in the image file
 function first_partition_offset () {
-  local filename="$1"
-  local size_in_bytes
-  local size_in_sectors
-  local sector_size
-  local partition_start_sector
+  local filename="$1"       # Get the filename from the first argument
+  local size_in_bytes       # Variable to store the size in bytes
+  local size_in_sectors     # Variable to store the size in sectors
+  local sector_size         # Variable to store the sector size
+  local partition_start_sector   # Variable to store the start sector of the partition
+
   size_in_bytes=$(sfdisk -l -o Size -q --bytes "$1" | tail -1)
   size_in_sectors=$(sfdisk -l -o Sectors -q "$1" | tail -1)
   sector_size=$(( size_in_bytes / size_in_sectors ))
   partition_start_sector=$(sfdisk -l -o Start -q "$1" | tail -1)
+
   echo $(( partition_start_sector * sector_size ))
 }
-function add_drive () {
+
+function add_drive () { 
+
   local name="$1"
   local label="$2"
   local size="$3"
   local filename="$4"
   fallocate -l "$size"K "$filename"
+  
   echo "type=c" | sfdisk "$filename" > /dev/null
   
   local partition_offset
@@ -137,6 +157,7 @@ function add_drive () {
     mkdir "$mountpoint"
   fi
 }
+
 add_drive "arlo" "ARLO" "$ARLO_IMG_SIZE" "$ARLO_IMG_FILE" 
 
 echo "Storage IMG SUCCESS - 5/6" >> "$LOG_FILE"
@@ -146,10 +167,20 @@ echo "Storage IMG SUCCESS - 5/6" >> "$LOG_FILE"
 init_mass_storage="@reboot sudo sh $(pwd)/enable_mass_storage.sh $MAX_POWER"
 sync_clip_interval="*/1 * * * * sudo /bin/bash $(pwd)/sync_clips.sh"
 cleanup_clips_interval="0 0 * * * sudo /bin/bash $(pwd)/cleanup_clips.sh"
-( crontab -l 2>/dev/null | cat;  echo "$init_mass_storage" ) | crontab -
-( crontab -l 2>/dev/null | cat;  echo "$sync_clip_interval" ) | crontab -
-( crontab -l 2>/dev/null | cat;  echo "$cleanup_clips_interval" ) | crontab -
 
+# Add init_mass_storage to crontab
+( crontab -l 2>/dev/null | cat;  echo "$init_mass_storage" ) | crontab - \
+    || { echo "Failed to add init_mass_storage to crontab" >> "$LOG_FILE"; exit 1; }
+
+# Add sync_clip_interval to crontab
+( crontab -l 2>/dev/null | cat;  echo "$sync_clip_interval" ) | crontab - \
+    || { echo "Failed to add sync_clip_interval to crontab" >> "$LOG_FILE"; exit 1; }
+
+# Add cleanup_clips_interval to crontab
+( crontab -l 2>/dev/null | cat;  echo "$cleanup_clips_interval" ) | crontab - \
+    || { echo "Failed to add cleanup_clips_interval to crontab" >> "$LOG_FILE"; exit 1; }
+
+# Log the success message
 echo "Cronjob SUCCESS - 6/6" >> "$LOG_FILE"
 
 #################################################################
